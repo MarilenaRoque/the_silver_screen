a0ac98f87cdfd3ae9490ef1405b9110c9fd77899
MarilenaRoque <marilenas@id.uff.br>
2020-09-15 14:30:18 -0300

    Generate User controller
    set index for users
    set root
    add test view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:index]
+  root to: "users#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
