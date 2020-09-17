15a9c825e01641d576dd7750c6b897d8bda4e3ee
MarilenaRoque <marilenas@id.uff.br>
2020-09-17 12:10:28 -0300

    Set association built between category and article
    Add Form checkbox to receive categories
    Whitelisted categories list params as an array of ids
    Fake the categories_list as an attribute on article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,15 @@ class Article < ApplicationRecord
     belongs_to :author, class_name: "User"
     has_one_attached :image
     has_and_belongs_to_many :categories, class_name: "Category"
+
+    def categories_list
+        categories
+    end
+
+    def categories_list=(arr_ids)
+        arr_ids.each do |n| 
+            self.categories << Category.find(n.to_i)
+        end
+    end
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_215231) do
+ActiveRecord::Schema.define(version: 2020_09_16_222252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 2020_09_16_215231) do
   create_table "articles_categories", id: false, force: :cascade do |t|
     t.bigint "article_id", null: false
     t.bigint "category_id", null: false
+    t.index ["article_id"], name: "index_articles_categories_on_article_id"
+    t.index ["category_id"], name: "index_articles_categories_on_category_id"
   end
 
   create_table "categories", force: :cascade do |t|
