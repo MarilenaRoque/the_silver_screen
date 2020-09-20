5f9bf45c0ff8b0c4fff84d1db519a442062e2e59
MarilenaRoque <marilenas@id.uff.br>
2020-09-20 11:25:06 -0300

    Set destroy vote feature
    Set a helper method to return the path to destroy or create
    Set the views to load the right path
    Create routes to destroy
    Set methods to destroy

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -7,4 +7,9 @@ class VotesController < ApplicationController
        @vote = current_user.votes.build(article_id: params[:article_id])
        redirect_to '/categories' if @vote.save
     end
+
+    def destroy
+        @vote = current_user.votes.find_by(article_id: params[:article_id])
+        redirect_to '/categories' if @vote.destroy
+    end
 end
