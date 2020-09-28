8dcc4715738341c5e434d5fcc9c50c4517a5c17f
MarilenaRoque <marilenas@id.uff.br>
2020-09-28 19:09:24 -0300

    Built integration tests for votes and bookmarks
    Testing Bookmark index view
    Testing Bookmarg attributes
    Testing Feature main article on index page
    Testing votes attributes

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -1,11 +1,11 @@
 module BookmarksHelper
   def bookmark_button(article)
     if !article.bookmarks.where(user_id: current_user.id).exists?
-      link_to(image_tag('bookmark.svg', height: '32', width: '23'),
+      link_to(image_tag('bookmark.svg', height: '32', width: '23', class:'bookmark'),
               bookmarks_path(article_id: article.id, category_id: @category.id),
               method: :post).html_safe
     else
-      link_to(image_tag('unbookmark.svg', height: '32', width: '23'),
+      link_to(image_tag('unbookmark.svg', height: '32', width: '23', class:'bookmark'),
               bookmark_path(article_id: article.id, category_id: @category.id),
               method: :delete).html_safe
     end
